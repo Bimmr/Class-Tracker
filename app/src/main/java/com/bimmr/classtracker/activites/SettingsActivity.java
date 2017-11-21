ae5a874e47905285d418d25797630522baceedcf
Bimmr <Bimmr6696@gmail.com>
2017-11-21 10:55:22 -0500

    Started edit on settings

--- app/src/main/java/com/bimmr/classtracker/activites/SettingsActivity.java
+++ app/src/main/java/com/bimmr/classtracker/activites/SettingsActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bimmr.classtracker.Manager;
 import com.bimmr.classtracker.Preferences;
 import com.bimmr.classtracker.R;
 import com.bimmr.classtracker.activites.startup.StartActivity;
@@ -21,6 +23,12 @@ public class SettingsActivity extends BaseActivity {
             Preferences.remove("email","password");
 
             startActivity(new Intent(this, StartActivity.class));
+        });
+        ((TextView) findViewById(R.id.settings_changePassTitle)).setOnClickListener(click -> {
+            ((TextView) findViewById(R.id.settings_changePass)).callOnClick();
+        });
+        ((TextView) findViewById(R.id.settings_changePass)).setOnClickListener(click -> {
+            Toast.makeText(Manager.getContext(), "Change pass", Toast.LENGTH_SHORT).show();
         });
 
     }
